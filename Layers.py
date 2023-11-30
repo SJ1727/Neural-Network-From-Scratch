Fixed softmax test and added relu

Layers.py
@@ -2,54 +2,104 @@ import numpy as np
 
 class LinearLayer:
     def __init__(self, in_features, out_features):
-        self.weights = np.random.rand(in_features, out_features)
+        self.weights = np.random.randn(in_features, out_features).transpose()
+        print(self.weights.shape)
         self.biases = np.random.randn(out_features)
         self.weight_grad = None
         self.bias_grad = None
+        self.activations_gradient = None
+        self.activations = None
 
     def forward(self, x):
         x = np.matmul(self.weights, x)
         x = x + self.biases
+        self.activations = x
+        return x
+
+    def backward(self, grad):
+        self.bias_grad = grad
+
+        self.weight_grad = np.ones(self.weights.shape)
+        self.weight_grad = self.weight_grad * grad[:, np.newaxis]
+        self.weight_grad = self.weight_grad * self.activations[:, np.newaxis]
+
+        self.activations_gradient = np.matmul(self.weights.transpose(), grad)
+
+        return self.activations_gradient
 
 class Softmax:
     def __init__(self):
         self.grad = None
+        self.activations = None
 
     def forward(self, x):
         exp_matrix = np.exp(x)
+        self.activations = exp_matrix / np.sum(exp_matrix)
 
-        return exp_matrix / np.sum(exp_matrix)
+        return self.activations
 
-    def backward(self, x, grad):
-        self.grad = np.matmul(self.__jacobian(x), grad)
+    def backward(self, grad):
+        self.grad = np.matmul(self.__jacobian(self.activations), grad)
+
+        return self.grad
 
     def __jacobian(self, x):
-        x_new_axis = x[:, np.newaxis]
-        return -(np.ones((x.shape[0], x.shape[0])) * x_new_axis * x_new_axis.transpose() - np.diag(x))
+        return np.diag(x) - np.outer(x, x) 
+    
+    def jacobian(self, x):
+        return self.__jacobian(x)
 
 class MSELoss:
     def __init__(self):
         self.grad = None
+        self.loss = 0
 
     def forward(self, x, expected):
         loss = x - expected
         loss = np.square(loss)
+        self.loss = np.mean(loss)
 
-        return np.mean(loss)
+        return self.loss
     
     def backward(self, x, expected):
         self.grad = (2 / len(expected)) * (x - expected)
 
+        return self.grad
+
 class CrossEntropyLoss:
     def __init__(self):
         self.grad = None
+        self.loss = 0
+        self.activations = None
 
     def forward(self, x, expected):
+        self.activations = np.copy(x)
+
         log_x = np.log(x)
+        self.loss = -np.sum(log_x * expected)
 
-        return -np.sum(log_x * expected)
+        return self.loss
 
-    def backward(self, x, expected):
-        self.grad = -np.divide(expected, x)
+    def backward(self, expected):
+        self.grad = -np.divide(expected, self.activations)
 
+        return self.grad
 
+class Relu:
+    def __init__(self):
+        self.grad = None
+        self.activations = None
+
+    def forward(self, x):
+        relu_func = np.vectorize(lambda x: 0 if x < 0 else x)
+
+        self.activations = relu_func(x)
+
+        return self.activations
+    
+    def backward(self, grad):
+        relu_derivative_func = np.vectorize(lambda x: 0 if x < 0 else 1)
+
+        self.grad = relu_derivative_func(self.activations) * grad
+
+        return self.grad

test/LayersTest.py
@@ -33,26 +33,78 @@ def test_cross_entropy_forward():
 
     print(crit.forward(y_pred, y_true))
 
-def test_softmax_backward():
-    print("\nSoftmax Backward\n")
 
-    predicted1 = np.random.randn(5)
+def test_softmax_backward():
+    print("\n---Softmax Backward---\n")
+
+    x = np.random.randn(5)
     actual1 = np.array([0, 0, 0, 0, 1])
 
     crit = CrossEntropyLoss()
     softm = Softmax()
 
-    predicted1 = softm.forward(predicted1)
+    x = softm.forward(x)
+    crit.forward(softm.activations, actual1)
 
-    crit.backward(predicted1, actual1)
-    softm.backward(predicted1, crit.grad)
+    assert np.allclose(crit.activations, softm.activations)
+    assert np.allclose(x, softm.activations)
+
+    grad1 = crit.backward(actual1)
+    grad2 = softm.backward(grad1)
 
     # When used with cross entropy loss the gradient of the input values of the softmax function
     # are equal to the predicted result (the inputs after softmax is applied) minus the actual
     # values
-    assert np.allclose(softm.grad, predicted1 - actual1)
+    assert np.allclose(grad2, x - actual1)
+
+
+def test_linear_layer():
+    print("\n---Linear Layer---\n")
+    l1 = LinearLayer(10, 3)
+    softm = Softmax()
+    crit = CrossEntropyLoss()
+
+    data = np.random.randn(10)
+    expected = np.array([0, 0, 1])
+    x = data
+
+    x = l1.forward(x)
+    x = softm.forward(x)
+    x = crit.forward(x, expected)
+
+    g1 = crit.backward(expected)
+    g2 = softm.backward(g1)
+    g3 = l1.backward(g2)
+
+    # print(l1.weight_grad)
+
+def test_relu_forward():
+    print("\n---Relu forward---\n")
+    test1 = np.array([1, -1, 2, 0, -1])
+    expected1 = np.array([1, 0, 2, 0, 0])
+
+    test2 = np.array([[1, 2], [-1, 0]])
+    expected2 = np.array([[1, 2], [0, 0]])
+
+    relu = Relu()
+
+    result1 = relu.forward(test1)
+    result2 = relu.forward(test2)
+
+    assert np.allclose(result1, expected1)
+    assert np.allclose(result2, expected2)
 
 if __name__ == "__main__":
-    #test_softmax_forward()
+    test_cross_entropy_forward()
+    test_softmax_forward()
     test_softmax_backward()
-    #test_cross_entropy_forward()
+    test_relu_forward()
+    test_linear_layer()
+
+    # s = Softmax()
+    # t = np.array([1, 2, 3])
+    # g = np.array([1, 3, 4])
+
+    # j = s.jacobian(t)
+    # print(j)
+    # print(np.matmul(j, g))